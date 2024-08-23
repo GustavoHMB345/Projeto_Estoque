455812dc7c22456d477ed820b504553c3883b98c
GustavoHMB345 <gustavohx3bozz@gmail.com>
2024-08-23 12:38:17 -0300

    overwrite erros

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/auth_model.dart';
 import 'providers/app_state.dart';
-import 'pages/auth_page.dart';
+import 'pages/auth_page.dart'; 
 import 'pages/my_home_page.dart';
-import 'sqflite/user.dart'; 
 
 void main() {
-  AuthModel authModel = AuthModel();
-
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => authModel),
+        ChangeNotifierProvider(create: (context) => AuthModel()),
         ChangeNotifierProvider(create: (context) => AppState()),
       ],
       child: MyApp(),
@@ -23,37 +20,24 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final authModel = Provider.of<AuthModel>(context, listen: false);
-    final username = 'Gus';
-    final password = '1212';
-
-    return FutureBuilder(
-      future: authModel.signup(User(username: username, password: password)), 
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            title: 'Aplicação do estoque',
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-            ),
-            initialRoute: '/',
-            routes: {
-              '/': (context) => Consumer<AuthModel>(
-                builder: (context, authModel, child) {
-                  if (authModel.isAuthenticated) {
-                    return const MyHomePage();
-                  } else {
-                    return AuthPage();
-                  }
-                },
-              ),
-              '/home': (context) => const MyHomePage(),
-              '/login': (context) => AuthPage(),
-            },
-          );
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
+    return MaterialApp(
+      title: 'Aplicação do estoque',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Consumer<AuthModel>(
+          builder: (context, authModel, child) {
+            if (authModel.isAuthenticated) {
+              return const MyHomePage();
+            } else {
+              return AuthPage();
+            }
+          },
+        ),
+        '/home': (context) => const MyHomePage(),
+        '/login': (context) => AuthPage(),
       },
     );
   }
--- lib/providers/auth_model.dart
+++ lib/providers/auth_model.dart
@@ -1,29 +1,22 @@
 import 'package:flutter/foundation.dart';
-import '../sqflite/database_helper.dart';
-import '../sqflite/user.dart';
 
-class AuthModel with ChangeNotifier {
-  final DatabaseHelper _databaseHelper = DatabaseHelper();
+class AuthModel extends ChangeNotifier {
   bool _isAuthenticated = false;
 
-  Future<void> login(String username, String password) async {
-    final user = await _databaseHelper.getUserByUsernameAndPassword(username, password);
-    if (user != null) {
+  bool get isAuthenticated => _isAuthenticated;
+
+  void login(String username, String password) {
+    if (username == 'adm' && password == '12') {
       _isAuthenticated = true;
+      notifyListeners();
+    } else {
+      _isAuthenticated = false;
       notifyListeners();
     }
   }
 
-  Future<void> signup(User user) async {
-    await _databaseHelper.insertUser(user);
-    _isAuthenticated = true;
-    notifyListeners();
-  }
-
-  Future<void> logout() async {
+  void logout() {
     _isAuthenticated = false;
     notifyListeners();
   }
-
-  bool get isAuthenticated => _isAuthenticated;
 }
